0490a25d8689e90cdee66a3a300e188b47a9e3ad
spw <553648727@163.com>
2021-06-30 23:29:19 +0800

    -- webpack初次使用

---
+++ webpack使用/01-webpack起步使用/src/main.js
@@ -0,0 +1,11 @@
+
+//var {add, mul} = requires('./mathUtils.js');
+
+import {add, mul} from './js/mathUtils.js';
+
+import css from './css/normal.css';
+
+import less from './css/special.less';
+
+console.log(add(10, 20));
+console.log(mul(10, 20));
---
+++ webpack使用/01-webpack起步使用/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/i,
+        //use: ['style-loader', 'css-loader', 'less-loader'],
+        use: [
+            {loader: 'style-loader'},
+            {loader: 'css-loader'},
+            {loader: 'less-loader'},
+        ]
+      }
+    ]
+  }
+};
